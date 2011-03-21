a232b382296fbf8ad599286ac8780b96d067a921
Grepsy <robertmassa@gmail.com>
2011-03-21 23:41:43 +0100

    Added basic sinatra app to rack.

--- lib/cahoots.rb
+++ lib/cahoots.rb
@@ -1,5 +1,11 @@
 require 'cahoots/server'
+require 'sinatra/base'
 
 module Cahoots
-  
 end # Cahoots
+
+class MyApp < Sinatra::Base
+  get '/workspaces/:workspace' do
+    "Hello #{params[:workspace]}"
+  end
+end
